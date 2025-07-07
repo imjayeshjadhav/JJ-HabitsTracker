0fdcc0a0d2d6ff420a869e38b0e7814c0ea78ddd
imjayeshjadhav <14jayeshjadhav@gmail.com>
2025-07-07 13:41:00 +0530

    Data fetching

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,35 @@
+import { DATABASE_ID, databases, HABITS_COLLECTION_ID } from "@/lib/appwrite";
 import { useAuth } from "@/lib/auth-context";
+import { Habit } from "@/types/database.type";
 import { Link } from "expo-router";
+import { useEffect, useState } from "react";
 import { View } from "react-native";
+import { Query } from "react-native-appwrite";
 import { Button } from "react-native-paper";
 
 export default function Index() {
 
-  const {signOut} = useAuth()
+  const {signOut, user} = useAuth()
+
+  const [habits, setHabits] = useState<Habit[]>()
+
+  useEffect(()=>{
+    fetchHabits()
+  },[user])
+
+  const fetchHabits = async () =>{
+    try {
+      const response = await databases.listDocuments(
+        DATABASE_ID, 
+        HABITS_COLLECTION_ID,
+        [Query.equal("user_id", user?.$id ?? "")]
+      )
+      console.log(response.documents)
+      setHabits(response.documents as Habit[])
+    } catch (error) {
+      
+    }
+  }
 
   return (
     <View
